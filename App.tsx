c75fca48c7dcfab396ad8191ff863b210c7f439e
Ghaith <ghaith990000@gmail.com>
2024-03-15 23:50:49 +0300

    create a separate DayListItem component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-
+import DayListItem from './src/components/core/DayListItem';
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
