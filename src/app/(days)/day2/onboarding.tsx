58afb40745ae431c448fe731010b4e14a01fb30a
Ghaith <ghaith990000@gmail.com>
2024-03-16 17:24:07 +0300

    added animation to the onboarding page

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -4,20 +4,24 @@ import { Stack, router } from 'expo-router';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { GestureDetector, Gesture, Directions } from 'react-native-gesture-handler';
+import Animated, {FadeIn, FadeOut,SlideInLeft, BounceInRight, BounceOutLeft, SlideOutRight, SlideOutLeft, SlideInRight} from 'react-native-reanimated';
 
 const onboardingSteps = [
     {
+        id: 1,
         icon: "snowflake",
         title: "Welcome #DEVemeber",
         description: "Daily React Native tutorials during December",
     },
     {
+        id: 2,
         icon: "people-arrows",
         title: "Learn and grow together",
         description: "Learn by building 24 projects with React Native and Expo",
     },
     {
-        icon: "people-arrows",
+        id: 3,
+        icon: "book-reader",
         title: "Education for Children",
         description: "Contribute to the fundraiser \"Education for Children to help Save the Children in their effort of providing education to every child",
     },
@@ -50,13 +54,14 @@ const OnboardingScreen = () => {
         setScreenIndex(0);
         router.back();
     }
-    const swipeForward = Gesture.Fling().direction(Directions.LEFT)
+    const swipeForward = Gesture.Fling().runOnJS(true).direction(Directions.LEFT)
     .onEnd((event) => {
         console.log("Fling end", event);
         onContinue();
     });
 
-    const swipeBack = Gesture.Fling().direction(Directions.RIGHT).onEnd((event) => {
+    
+    const swipeBack = Gesture.Fling().runOnJS(true).direction(Directions.RIGHT).onEnd((event) => {
         console.log('Fling end: ', event.state);
         onBack();
     });
@@ -66,19 +71,21 @@ const OnboardingScreen = () => {
     <SafeAreaView style={styles.page}>
         <Stack.Screen options={{headerShown: false}} />
         <StatusBar style='light' />
+        <View style={styles.stepIndicatorContainer}>
+            {onboardingSteps.map((step, index) => (
+                <View key={step.id} style={[styles.stepIndicator, {backgroundColor: index === screenIndex ? '#CEF202' : 'gray'}]} />
+            ))}
+        </View>
         <GestureDetector gesture={swipes}>
-            <View style={styles.pageContent}>
-                <View style={styles.stepIndicatorContainer}>
-                    {onboardingSteps.map((step, index) => (
-                        <View style={[styles.stepIndicator, {backgroundColor: index === screenIndex ? '#CEF202' : 'gray'}]} />
-                    ))}
-                </View>
-                <FontAwesome5 style={styles.image} name={data.icon} size={150} color="#CEF202" />
+            <View style={styles.pageContent} key={data.id}>
+                <Animated.View entering={FadeIn} exiting={FadeOut}>
+                    <FontAwesome5 style={styles.image} name={data.icon} size={150} color="#CEF202" />
+                </Animated.View>
                 <View style={styles.footer}>
-                    <Text style={styles.title}>{data.title}</Text>
-                    <Text style={styles.description}>
+                    <Animated.Text entering={SlideInRight.duration(500)} exiting={SlideOutLeft} style={styles.title}>{data.title}</Animated.Text>
+                    <Animated.Text entering={SlideInRight.delay(150)} exiting={SlideOutLeft.delay(150)} style={styles.description}>
                         {data.description}
-                    </Text>
+                    </Animated.Text>
                     <View style={styles.buttonsRow}>
                         <Text onPress={endOnboarding} style={styles.buttonText}>Skip</Text>
                         <Pressable onPress={onContinue} style={styles.button}>
